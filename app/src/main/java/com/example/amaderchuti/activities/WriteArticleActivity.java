7ef036f4857157e18332a814ccdf81e3bc220d4f
SayantanBera <sayantan.bera@shyamsteel.com>
2024-05-17 11:01:31 +0530

    Testing

---
+++ app/src/main/java/com/example/amaderchuti/activities/WriteArticleActivity.java
@@ -0,0 +1,16 @@
+package com.example.amaderchuti.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.amaderchuti.R;
+
+public class WriteArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_article);
+    }
+}
