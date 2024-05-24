f2a605f382b543c2c5119bcf47b0a47b4a472816
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2024-05-24 20:43:28 +0530

    1. Homepage UI in progress (Meeting with team-mate left for finalizing) 2. Article preview page is completed. (Require a team meeting for finalizing)

--- app/src/main/java/com/example/amaderchuti/activities/MainActivity.java
+++ app/src/main/java/com/example/amaderchuti/activities/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.article_preview_page);
+        setContentView(R.layout.activity_main);
     }
 }
 
